499fa0050c298fe4346efe40c2ccef6301c70c66
BadMadDev <marcus@dammann-web.eu>
2015-07-01 17:09:21 +0200

    First test projects for Azure Mobile Services.

---
+++ ramp_festival/ramp_festival.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,8 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ramp_festival
+{
+    public sealed partial class MainPage : Page
+    {
+    }
+}
---
+++ ramp_festivalService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ramp_festivalService.WebApiApplication" Language="C#" %>
