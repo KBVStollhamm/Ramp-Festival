f5079c943fa06c1f78ee64fd5606493176896d17
Marcus Dammann <marcus@dammann-web.eu>
2015-07-07 22:23:42 +0200

    Started with public web site.

---
+++ mad.Ramp-Festival.Web.Public/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mad.Ramp_Festival.Web.Public.MvcApplication" Language="C#" %>
---
+++ mad.Ramp-Festival.Web.Public/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mad.Ramp_Festival.Web.Public.Startup))]
+namespace mad.Ramp_Festival.Web.Public
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
