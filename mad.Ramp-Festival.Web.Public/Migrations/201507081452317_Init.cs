de9ebe87e3ae71fde2eedfc16806efd790074e5f
BadMadDev <marcus@dammann-web.eu>
2015-07-08 17:57:40 +0200

    Added auth functinality with DisplayName attribute.

---
+++ mad.Ramp-Festival.Web.Public/Migrations/201507081452317_Init.cs
@@ -0,0 +1,16 @@
+namespace mad.Ramp_Festival.Web.Public.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Init : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- mad.Ramp-Festival.Web.Public/Models/IdentityModels.cs
+++ mad.Ramp-Festival.Web.Public/Models/IdentityModels.cs
@@ -6,28 +6,34 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace mad.Ramp_Festival.Web.Public.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
-    }
+	// You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+	public class ApplicationUser : IdentityUser
+	{
+		public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+		{
+			// Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+			var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+			
+			// Add custom user claims here
+			if (!string.IsNullOrEmpty(this.DisplayName))
+				userIdentity.AddClaim(new Claim("DisplayName", this.DisplayName));
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
+			return userIdentity;
+		}
 
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+		public string DisplayName { get; set; }
+	}
+
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext()
+			: base("DefaultConnection", throwIfV1Schema: false)
+		{
+		}
+
+		public static ApplicationDbContext Create()
+		{
+			return new ApplicationDbContext();
+		}
+	}
 }
