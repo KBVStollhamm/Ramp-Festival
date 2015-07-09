b6645e0098cc6d764d84fb9da12f63ce81c9a6f7
Marcus Dammann <marcus@dammann-web.eu>
2015-07-09 07:20:16 +0200

    Settings for database migration revised.

--- mad.Ramp-Festival.Web.Public/Migrations/Configuration.cs
+++ mad.Ramp-Festival.Web.Public/Migrations/Configuration.cs
@@ -1,33 +1,31 @@
 namespace mad.Ramp_Festival.Web.Public.Migrations
 {
-	using System;
-	using System.Data.Entity;
-	using System.Data.Entity.Migrations;
-	using System.Linq;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
 
-	internal sealed class Configuration : DbMigrationsConfiguration<mad.Ramp_Festival.Web.Public.Models.ApplicationDbContext>
-	{
-		public Configuration()
-		{
-			AutomaticMigrationsEnabled = true;
-			AutomaticMigrationDataLossAllowed = true;
-			ContextKey = "mad.Ramp_Festival.Web.Public.Models.ApplicationDbContext";
-		}
+    internal sealed class Configuration : DbMigrationsConfiguration<mad.Ramp_Festival.Web.Public.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
 
-		protected override void Seed(mad.Ramp_Festival.Web.Public.Models.ApplicationDbContext context)
-		{
-			//  This method will be called after migrating to the latest version.
+        protected override void Seed(mad.Ramp_Festival.Web.Public.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
 
-			//  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-			//  to avoid creating duplicate seed data. E.g.
-			//
-			//    context.People.AddOrUpdate(
-			//      p => p.FullName,
-			//      new Person { FullName = "Andrew Peters" },
-			//      new Person { FullName = "Brice Lambson" },
-			//      new Person { FullName = "Rowan Miller" }
-			//    );
-			//
-		}
-	}
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
 }
