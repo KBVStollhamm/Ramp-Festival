fbf98275b38899c8df2f64ddc3bc9f50139d70af
BadMadDev <marcus@dammann-web.eu>
2015-07-22 14:14:15 +0200

    Added page and action for Geschicklichkeitsbosseln. Content is still missing.

--- mad.Ramp-Festival.Web.Public/Controllers/ContestController.cs
+++ mad.Ramp-Festival.Web.Public/Controllers/ContestController.cs
@@ -19,6 +19,11 @@ namespace mad.Ramp_Festival.Web.Public.Controllers
 			return View();
 		}
 
+		public ActionResult GBosseln()
+		{
+			return View();
+		}
+
 		public ActionResult Luftkegeln()
 		{
 			return View();
