10211eafbda129cacbe7c2f7b6cf5d40c1f74818
Marcus Dammann <marcus@dammann-web.eu>
2015-07-30 18:04:45 +0200

    Added intial Registration Bounded Context, Contest Execution App, some infratructural work.

---
+++ Infrastructure/Infrastructure.EventSourcing/IVersionedEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Messaging;
+
+namespace Infrastructure.EventSourcing
+{
+	/// <summary>
+	/// Represents an event message that belongs to an ordered event stream.
+	/// </summary>
+	public interface IVersionedEvent : IEvent
+	{
+		/// <summary>
+		/// Gets the version or order of the event in the stream.
+		/// </summary>
+		int Version { get; }
+	}
+}
