703d38303a69b7a8e6c785d268a187df6d241e4b
Marcus Dammann <marcus@dammann-web.eu>
2015-08-07 15:03:44 +0200

    Added a test app for testing animation displaying next player. Just a temp project until it is implemented in Registration project.

---
+++ TestApp/TestApp/PlayerControl.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TestApp
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public partial class PlayerControl : UserControl
+	{
+		Storyboard _hideBorder;
+		Storyboard _showBorder;
+
+		public PlayerControl()
+		{
+			InitializeComponent();
+
+			PlayerViewModel viewModel = this.DataContext as PlayerViewModel;
+			viewModel.PlayerName = "Player 1";
+
+			viewModel.PropertyChanging += ViewModel_PropertyChanging;
+
+			_hideBorder = Resources["HideBorder"] as Storyboard;
+			_hideBorder.Completed += HideBorder_Completed;
+
+			_showBorder = Resources["ShowBorder"] as Storyboard;
+		}
+
+		private void ViewModel_PropertyChanging(object sender, System.ComponentModel.PropertyChangingEventArgs e)
+		{
+			Dispatcher.BeginInvoke((Action)(() => BeginStoryboard(_hideBorder)), DispatcherPriority.Send);
+			this.Wait(TimeSpan.FromSeconds(0.5));
+		}
+
+		private void HideBorder_Completed(object sender, EventArgs e)
+		{
+			BeginStoryboard(_showBorder);
+		}
+	}
+
+	public static class DispatcherObjectExtensions
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="dispatcherObject"></param>
+		/// <param name="timeSpan"></param>
+		public static void Wait(this DispatcherObject dispatcherObject, TimeSpan timeSpan)
+		{
+			Wait(dispatcherObject, timeSpan.Ticks);
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="dispatcherObject"></param>
+		/// <param name="ticks"></param>
+		public static void Wait(this DispatcherObject dispatcherObject, long ticks)
+		{
+			long dtEnd = DateTime.Now.AddTicks(ticks).Ticks;
+
+			while (DateTime.Now.Ticks < dtEnd)
+			{
+				dispatcherObject.Dispatcher.Invoke(DispatcherPriority.Background, (DispatcherOperationCallback)delegate(object unused) { return null; }, null);
+			}
+		}
+	}
+}
---
+++ TestApp/TestApp/PlayerViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Threading;
+
+namespace TestApp
+{
+	public class PlayerViewModel : INotifyPropertyChanged, INotifyPropertyChanging 
+	{
+		Random _rand = new Random(Environment.TickCount);
+		List<string> _playerNames;
+		Timer _timer;
+
+		public PlayerViewModel()
+		{
+			// alle 10 sekunden 
+			_timer = new Timer(5000);
+			_timer.Elapsed += Timer_Elapsed;
+			_timer.Start();
+
+			_playerNames = Enumerable.Range(0, 50).Select(i => "Player " + i).ToList();
+		}
+
+		private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			PlayerName = _playerNames[_rand.Next(0, _playerNames.Count)];
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (!ReferenceEquals(handler, null))
+			{
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+		public event PropertyChangingEventHandler PropertyChanging;
+
+		protected virtual void OnPropertyChanging(string propertyName)
+		{
+			PropertyChangingEventHandler handler = PropertyChanging;
+			if (!ReferenceEquals(handler, null))
+			{
+				handler(this, new PropertyChangingEventArgs(propertyName));
+			}
+		}
+
+		private string _playerName;
+		public string PlayerName
+		{
+			get { return _playerName; }
+			set
+			{
+				if (_playerName == value)
+					return;
+
+				OnPropertyChanging("PlayerName");
+				_playerName = value;
+				OnPropertyChanged("PlayerName");
+			}
+		}
+	}
+}
