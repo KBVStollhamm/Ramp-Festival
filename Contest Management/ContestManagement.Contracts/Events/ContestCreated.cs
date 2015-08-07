4f90573e00ba913c6f823befc1943fb69e38c49f
Marcus Dammann <marcus@dammann-web.eu>
2015-08-07 12:59:38 +0200

    Added contract files.

---
+++ Contest Management/ContestManagement.Contracts/Events/ContestCreated.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContestManagement.Events
+{
+	/// <summary>
+	/// Event published whenever a new contest is created.
+	/// </summary>
+	public class ContestCreated : ContestEvent
+	{
+	}
+}
---
+++ Contest Management/ContestManagement.Contracts/Events/ContestEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Messaging;
+
+namespace ContestManagement.Events
+{
+	/// <summary>
+	/// Base class for contest-related events, containing 
+	/// all the contest information.
+	/// </summary>
+	public abstract class ContestEvent : IEvent
+	{
+		public Guid SourceId { get; set; }
+
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Location { get; set; }
+		public string Slug { get; set; }
+		public string Tagline { get; set; }
+		public string TwitterSearch { get; set; }
+
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+	}
+}
